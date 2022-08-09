23cd903b90bc0df67f8aaee853e8ab33d7135029
Ael99 <103473988+Ael99@users.noreply.github.com>
2022-08-09 17:10:04 +0700

    Update Sudoku.py
    Complete

--- Sudoku.py
+++ Sudoku.py
@@ -1,12 +1,12 @@
-board = [[0,0,1,3,2,5,9,7,8],
-         [0,0,0,0,0,8,5,3,1],
-         [0,0,8,0,0,0,4,2,0],
-         [7,0,4,0,5,0,8,0,0],
-         [0,0,3,0,0,0,1,6,0],
-         [8,5,0,0,3,0,7,0,0],
-         [0,6,5,0,0,0,2,0,0],
-         [4,8,7,1,0,2,3,0,0],
-         [0,9,2,0,0,3,6,8,0]]
+board = [[0,0,0,0,1,0,0,0,4],
+         [4,0,0,0,9,0,1,7,2],
+         [2,1,0,0,8,0,9,0,0],
+         [0,3,4,7,2,8,6,0,0],
+         [0,0,0,1,0,0,3,4,7],
+         [7,0,1,0,0,0,2,0,0],
+         [1,2,0,0,0,0,4,0,0],
+         [0,0,5,8,0,2,0,0,0],
+         [8,7,0,9,0,1,0,2,0]]
 def print_board():
     #print first line
     print('----' * len(board) + '-')
@@ -55,9 +55,11 @@ def solve(board):
                 return False
     return True                    
 
-print('Before')    
+print('Before !')    
 print_board()
 
-print('After')
-solve(board)
-print_board()
+if solve(board):
+    print('After !')
+    print_board()
+else:
+    print('Can not solve this. Sorry x.x !!!')
